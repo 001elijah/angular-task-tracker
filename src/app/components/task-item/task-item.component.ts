34f4acf4b1ccaad1943c5376ac00297753d4be8d
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-08-04 12:16:38 +0300

    ADD: render tasks with task-item, add angular-fontawesome

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Task } from '../../Task';
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
   styleUrls: ['./task-item.component.css']
 })
 export class TaskItemComponent {
-
+  @Input() task: Task;
 }
